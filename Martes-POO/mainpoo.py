07d075800ebb3fc323866c589a7431349721cf70
Amairani Vazquez <60655527+amairanii1608@users.noreply.github.com>
2023-06-13 21:01:49 -0600

    Add files via upload

---
+++ Martes-POO/mainpoo.py
@@ -0,0 +1,34 @@
+from cosas import Alumno
+
+def main():
+    """
+    al1 = Alumno()
+    print(al1)
+    al2 = Alumno()
+    print(al1.facultad)
+    print(al2.facultad)
+    print(Alumno.facultad)
+    #OJO
+    print("------------------")
+    Alumno.facultad = "FES Aragon EDOMEX"
+    print (al1. facultad)
+    print (al2. facultad)
+    print (Alumno. facultad)
+    print("Un vistazo a los objetos")
+    print(vars(al1))
+    print(vars(al2))
+    print("Modificar atributos públicos")
+    al1.edad = 999
+    print(vars(al1))
+    print(vars(al2))
+    """
+    
+    al1 = Alumno("Diego", 19, "ICO")
+    al2 = Alumno("Montse", 20, "Derecho")
+    al1.__edad = 333
+    print(vars(al1))
+    print(al1.__edad)
+    print(vars(al1))
+    
+    
+main()
